bb3d636e4c3fb57f57cdb959cad86a74dd6aece4
liuguolin <liuguolin@nowcoder.com>
2022-10-06 23:13:10 +0800

    添加主页星球旋转特效

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,22 @@
 <template>
-  <div class="home">
-    <div class="page">
-		<div class="content">
-		  	<div class="title">HI , I AM Gellen Liu</div>
-		    <div class="sub-title">welcome to my world</div>
-		    <div class="start-btn" @click="start">start</div>
-		</div>
-		<div class="img-box">
-			<img src="../../public/man.png"/>
-		</div>
-		<div class="fly">
-			<div class="left-wing"></div>
-			<img src="../../public/qihang.png"/>
-			<div class="right-wing"></div>
-		</div>
-    <div id="blackHall" class="black-hall">
-      <img v-if="showBlackHall" :key="freshDom" src="../../public/blackhole2019_pc.png"/>
+    <div class="home">
+        <div class="page">
+            <div class="content">
+                <div class="title">HI , I AM Gellen Liu</div>
+                <div class="sub-title">welcome to my world</div>
+                <div class="start-btn" @click="start">start</div>
+            </div>
+            <div class="img-box">
+                <img src="../../public/man.png" />
+                <div class="ball-wrap">
+                <div class="planet">
+                    <div class="ball" @mouseover="stopRotate" @mouseleave="startRotate" />
+                </div>
+            </div>
+            </div>
+            
+        </div>
     </div>
-<!-- 	<div class="ball-box">
-			<div class="ball"></div>
-		</div> -->
-		<!-- <div class="trans">
-			
-		</div> -->
-		 
- 
-	</div>
-  </div>
 </template>
 
 <script lang="ts">
@@ -34,27 +24,43 @@ import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+    components: {
+        HelloWorld
+    }
 })
 export default class Home extends Vue {
-  showBlackHall = false;
-  freshDom = Symbol()
+    showBlackHall = false;
+    freshDom = Symbol();
 
-  start() {
-    let that = this;
-    that.$router.push('/projectList')
+    start() {
+        let that = this;
+        that.$router.push('/projectList');
 
-    // this.showBlackHall = true;
-    // let blackHall = document.getElementById('blackHall');
-    // console.log(blackHall)
-    // blackHall?.addEventListener('animationend', ()=> {
-    //   this.showBlackHall = false;
-    //   that.freshDom = Symbol();
-    //   that.$router.push('/projectList')
-    // }, false)
-  }
+        // this.showBlackHall = true;
+        // let blackHall = document.getElementById('blackHall');
+        // console.log(blackHall)
+        // blackHall?.addEventListener('animationend', ()=> {
+        //   this.showBlackHall = false;
+        //   that.freshDom = Symbol();
+        //   that.$router.push('/projectList')
+        // }, false)
+    }
+    startRotate() {
+      let planet = document.querySelector('.planet');
+      let ball = document.querySelector('.ball');
+      if(planet && ball) {
+        planet.style.animationPlayState = 'running';
+        ball.style.animationPlayState = 'running';
+      }
+    }
+    stopRotate() {
+      let planet = document.querySelector('.planet');
+      let ball = document.querySelector('.ball');
+      if(planet && ball) {
+        planet.style.animationPlayState = 'paused';
+        ball.style.animationPlayState = 'paused';
+      }
+    }
 }
 </script>
 <style lang="scss" scoped src="@/styles/home.scss"></style>
