e76ba41a943343dc0c307258d3ba0d446cef3858
liuguolin <liuguolin@nowcoder.com>
2022-10-16 22:43:26 +0800

    feat: 主页添加星空效果

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="home">
+        <div id="sky-header"></div>
         <div class="page">
             <div class="content">
                 <div class="title" data-text="HI , I AM Gellen Liu">HI , I AM Gellen Liu</div>
@@ -9,12 +10,11 @@
             <div class="img-box">
                 <img src="../../public/man.png" />
                 <div class="ball-wrap">
-                <div class="planet">
-                    <div class="ball" @mouseover="stopRotate" @mouseleave="startRotate" />
+                    <div class="planet">
+                        <div class="ball" @mouseover="stopRotate" @mouseleave="startRotate" />
+                    </div>
                 </div>
             </div>
-            </div>
-            
         </div>
     </div>
 </template>
@@ -29,8 +29,13 @@ import HelloWorld from '@/components/HelloWorld.vue';
     }
 })
 export default class Home extends Vue {
+
     showBlackHall = false;
     freshDom = Symbol();
+
+    mounted() {
+        this.starSky();
+    }
 
     start() {
         let that = this;
@@ -46,20 +51,39 @@ export default class Home extends Vue {
         // }, false)
     }
     startRotate() {
-      let planet = document.querySelector('.planet');
-      let ball = document.querySelector('.ball');
-      if(planet && ball) {
-        planet.style.animationPlayState = 'running';
-        ball.style.animationPlayState = 'running';
-      }
+        let planet = document.querySelector('.planet');
+        let ball = document.querySelector('.ball');
+        if (planet && ball) {
+            planet.style.animationPlayState = 'running';
+            ball.style.animationPlayState = 'running';
+        }
     }
     stopRotate() {
-      let planet = document.querySelector('.planet');
-      let ball = document.querySelector('.ball');
-      if(planet && ball) {
-        planet.style.animationPlayState = 'paused';
-        ball.style.animationPlayState = 'paused';
-      }
+        let planet = document.querySelector('.planet');
+        let ball = document.querySelector('.ball');
+        if (planet && ball) {
+            planet.style.animationPlayState = 'paused';
+            ball.style.animationPlayState = 'paused';
+        }
+    }
+    starSky() {
+        let sky = document.getElementById('sky-header');
+        var screenW = sky.clientWidth;
+        var screenH = sky.clientHeight;
+        for (var i = 0; i < 40; i++) {
+            var span = document.createElement('span');
+            span.className = 'star'
+            sky.appendChild(span);
+            var x = Math.random() * screenW;
+            var y = Math.random() * screenH;
+            span.style.left = x + 'px';
+            span.style.top = y + 'px';
+            var scale = Math.random() * 1.5;
+            let rotate = -Math.random() * 20;
+            span.style.transform = 'scale(' + scale + ', ' + scale + ') rotate(' + rotate + 'deg)';
+            var rate = Math.random() * 5.5;
+            span.style.animationDelay = rate + 's';
+        }
     }
 }
 </script>
