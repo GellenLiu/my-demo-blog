a0dc6bb7533fff7adeb861862361dc488f785eb6
liuguolin <liuguolin@nowcoder.com>
2022-11-13 00:13:41 +0800

    项目目录页面优化

--- src/views/projectList.vue
+++ src/views/projectList.vue
@@ -1,12 +1,36 @@
 <template>
     <div class="projectList">
+        <div class="header">
+            <div class="go-back" @click="goHome">
+                <button class="learn-more">
+                    <span class="circle" aria-hidden="true">
+                        <span class="icon arrow"></span>
+                    </span>
+                    <span class="button-text">Back</span>
+                </button>
+            </div>
+        </div>
         <div class="list-wrap">
-            <div class="list-item" @click="goRouter(item.path, key)" v-for="(item, key) in itemList" :key="key">
+            <div
+                class="list-item"
+                @click="goRouter(item.path, key)"
+                v-for="(item, key) in itemList"
+                :key="key"
+            >
                 <div :class="index == key ? 'light-animate' : ''">
                     <div class="item-label">{{ item.label }}</div>
                     <!-- <div class="item-desc">{{ item.desc }}</div> -->
                 </div>
                 <!-- </div> -->
+            </div>
+        </div>
+        <div class="footer">
+            <div class="g-container">
+                <div class="g-wrap">
+                    <div class="g-footer">
+                        <div v-for="(item, index) in 200" :key="index" class="g-bubble"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -43,10 +67,15 @@ export default class TemplateCnp extends Vue {
         let that = this;
         that.clicked = true;
         that.index = index;
-        setTimeout(()=>{
+        setTimeout(() => {
             that.$router.push(path);
-        }, 200)
+        }, 200);
     }
+    goHome() {
+        let that = this;
+        that.$router.push('/');
+    }
+
 }
 </script>
 <style lang="scss" scoped src="@/styles/projectList.scss"></style>
